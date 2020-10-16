039e46be60c73517183dfe366e0c3f72aead96e1
jyoti-wp <sim_s006@icloud.com>
2020-10-16 17:00:51 +0530

    Dumping

---
+++ Exp-dump.php
@@ -0,0 +1,14 @@
+                <!-- Example for Dumping -->
+<!-- var_dump -->
+
+
+<!-- we use it to go ahead and get the value by dumping the data  -->
+
+
+
+<!-- lets define a variable  -->
+<!-- 
+$users = ['Jyoti' => 27, 'Priya' => 25];
+
+
+var_dump($users) -->

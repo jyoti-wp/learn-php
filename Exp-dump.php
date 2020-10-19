7ae7471aeb9fe22d9fc5ab60141626898c4f42dd
jyoti-wp <sim_s006@icloud.com>
2020-10-19 15:15:57 +0530

    funtion without declaring varialble or array

--- Exp-dump.php
+++ Exp-dump.php
@@ -93,8 +93,67 @@ var_dump($users) -->
 
 // lets check other variations of funtion 
 
-$fullName = funtion ($firstName, $lastName, $seperator = ' ') {
-    return "{$firstName}{$seperator}{$lastName}";
-};
+// $fullName = funtion ($firstName, $lastName, $seperator = ' ') {
+//     return "{$firstName}{$seperator}{$lastName}";
+// };
 
-echo $fullName('Joey', 'Green');
+// echo $fullName('Joey', 'Green');
+
+
+// lets bulid a funtion that calculates any sum 
+
+// we need list of numbers that add up 
+
+// funtion add($num1, $num2, $num3) {
+//     return $num1 + $num2 + $num3;
+// }   
+
+// echo add(5 + 10 + 10);
+
+
+// this works but its not very flexible
+// for example if we later have to add any number well have to go to the file where we added the numbers
+// whate we can do is we can pass the numbers into an array rather than directly into the funtion 
+
+
+// example 
+
+// $numbers = [5, 10, 15];
+
+// function add(array $numbers) {
+//     $total = 0;
+// }
+
+// foreach ($numbers as $number) {
+//     $total += $numbers;
+// }
+
+// return $total;
+
+// echo add($numbers);
+// // you can also pass the array directly without having to declare the variable 
+
+// example 
+// instead of writing 
+
+
+// function add(array $numbers) {
+//     $total = 0;
+// }
+
+// $numbers = [5, 10, 15];
+// echo add($numbers);
+
+// or (this is without having to declare the variable)
+
+// function add(array $numbers) {
+//     $total = 0;
+// }
+
+// echo add([5, 10, 15]);
+
+
+
+
+
+

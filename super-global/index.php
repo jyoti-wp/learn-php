05cf5e714efeaad63fa1eb9d7c57389944fc68f9
jyoti-wp <sim_s006@icloud.com>
2020-10-21 14:26:01 +0530

    ternary update

--- super-global/index.php
+++ super-global/index.php
@@ -1,9 +1,34 @@
 <?php 
 
-$page = $_GET['Page'];
-$searchTerm = $_GET['search'];
-$pages = 10;
+// $page = $_GET['Page'];
+// $searchTerm = $_GET['search'];
+// $pages = 10;
 
-// now lets replace the items from page and serachterm 
+// // now lets replace the items from page and serachterm 
 
-echo '<h3>Searching for: ' . $searchTerm . '</h3>';
+// echo '<h3>Searching for: ' . $searchTerm . '</h3>';
+
+
+// The ternary operator 
+
+// It will show us how to set any page to default one in case if the server is not working or from help page break 
+// we use ternary operator 
+// example
+
+// $page = 1;
+
+// if (isset($_GET['$page'])) {
+//     $page = $_GET['page'];
+// }
+
+// echo $page;
+
+
+// theres also an easier way 
+// Now we are gonna use the ternary operator
+// ternary operator is an inline if statement 
+// which means we can use isset bore the if statement 
+
+
+$page = isset($_GET['page']);
+

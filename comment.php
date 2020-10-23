60e297501e130c1c02b2cb5224e72ba1acd3c8bf
jyoti-wp <sim_s006@icloud.com>
2020-10-23 12:19:54 +0530

    update on commenting

---
+++ comment.php
@@ -0,0 +1,16 @@
+<?php
+
+            // commenting 
+// commenting is very important so that you can refer back to codes 
+// comments can be added above in line or below but not before the values 
+
+example 
+
+/*
+ * This fucntion takes string and stripes white space
+ * more text
+*/
+
+function removedWhiteSpace() {
+
+}
